6fdce922fcb56eaec3a6b2884df86b2a678e3702
oozeume <oozeume@gmail.com>
2024-02-05 12:29:52 +0900

    init : backend 기본 세팅

---
+++ backend/src/main/java/dev/service/newsscrap/NewsscrapApplication.java
@@ -0,0 +1,13 @@
+package dev.service.newsscrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsscrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsscrapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/service/newsscrap/NewsscrapApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.service.newsscrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsscrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
