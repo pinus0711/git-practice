fc8e3172be4dd969d39a7f5fcaa8fe55e01fea32
eheh10 <lime1pm@naver.com>
2024-02-06 09:29:25 +0900

    Feat react router dom, DefaultLayout

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsList from "./page/NewsList";
 
 function App() {
 
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<NewsList />} />
+      </Routes>
+    </Router>
   )
 }
 
