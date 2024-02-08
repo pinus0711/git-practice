963cd6c26c0b67c606ca86b2c4c78d26df5762ba
zzzdks760 <zzzdks760@naver.com>
2024-02-08 16:00:25 +0900

    feat: Scrap Paging

--- backend/src/main/java/dev/service/newsscrap/repository/ScrapRepository.java
+++ backend/src/main/java/dev/service/newsscrap/repository/ScrapRepository.java
@@ -1,8 +1,10 @@
 package dev.service.newsscrap.repository;
 
 import dev.service.newsscrap.entity.Scrap;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
-
+    Page<Scrap> findAllByMemberIdOrderByUpdatedTimeDesc(Long memberId, Pageable pageable);
 }
